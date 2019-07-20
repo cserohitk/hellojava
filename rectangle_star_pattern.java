83aa21cf0fd5a98a7a1c76de5d42999126cecd0a
cserohitk <53116531+cserohitk@users.noreply.github.com>
2019-07-20 18:55:13 +0530

    Add files via upload

---
+++ rectangle_star_pattern.java
@@ -0,0 +1,20 @@
+package demo;
+
+public class rectangle_star_pattern {
+	public static void main(String[] args)
+	{
+		int a,b;
+		int row = 5;//change the value of c to increase the size of cuboid
+	
+		for (a=1;a<=row;a++)
+		{
+			for (b=1;b<=row;b++)
+			{
+				System.out.print("*");
+				
+			}
+			
+			System.out.println();
+		}		
+	}
+}
